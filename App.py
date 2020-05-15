13e47b46a181033a53a997e7ddb48316470c8ebb
Zion Taber <62846363+zmt28@users.noreply.github.com>
2020-05-15 14:17:25 -0400

    Update App.py

--- App.py
+++ App.py
@@ -1,6 +1,32 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import mysql.connector as mysql
+import pandas as pd
+import csv
+
+df = pd.read_csv('Exercises.csv')
+
+with open('Exercises.csv') as f:
+    values = [tuple(line) for line in csv.reader(f)]
+
+db = mysql.connect(
+    host="localhost",
+    user="root",
+    passwd="bunny564",
+    database='Summer_Project'
+)
+
+cursor = db.cursor()
+
+query = "SELECT name FROM Exercises"
+
+cursor.execute(query)
+
+records = cursor.fetchall()
+x_values = []
+for record in records:
+    x_values.append(record)
 
 app = dash.Dash()
 
@@ -8,16 +34,16 @@ app = dash.Dash()
 app.layout = html.Div(children=[
     html.H1('Dash tutorials'),
     dcc.Graph(id='example',
-    figure={
-    'data' : [
-        {'x':[1,2,3,4,5],'y':[5,6,7,2,1], 'type': 'line', 'name':'boats'},
-        {'x':[1,2,3,4,5],'y':[8,3,2,3,5], 'type': 'bar', 'name':'cars'},
-    ],
-        'layout': {
-            'title': 'Basic Dash Example'
-        }
-    })
+              figure={
+                  'data': [
+                      {'x': x_values, 'y': [5, 6, 7, 2, 1], 'type': 'line', 'name': 'names'},
+
+                  ],
+                  'layout': {
+                      'title': 'Basic Dash Example'
+                  }
+              })
 ])
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     app.run_server(debug=True)
